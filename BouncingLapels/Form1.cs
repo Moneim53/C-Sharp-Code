70bf8beffec9675d28e88eea66c6bc6de7a0919b
Moneim53 <https://github.com/Moneim53>
2018-11-26 13:52:36 +0200

    https://github.com/Moneim53/C-Sharp-Code/tree/Bouncing_Lapels

---
+++ BouncingLapels/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingLapels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        labelBouncing[] Bouncer = new labelBouncing[3];
+
+        private void ToggleBouncing(int index, Label labelToBouncing)
+        {
+            if(Bouncer[index] == null)
+            {
+                Bouncer[index] = new labelBouncing();
+                Bouncer[index].MyLabel = labelToBouncing;
+            }
+            else
+            {
+                Bouncer[index] = null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToggleBouncing(0, label1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ToggleBouncing(1, label2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ToggleBouncing(2, label3);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if(Bouncer[i] != null)
+                {
+                    Bouncer[i].Move();
+                }
+            }
+        }
+    }
+}
--- BouncingLapels/Properties/AssemblyInfo.cs
+++ BouncingLapels/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Flashy!")]
+[assembly: AssemblyTitle("BouncingLapels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Flashy!")]
+[assembly: AssemblyProduct("BouncingLapels")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d0adfd96-838d-4e8c-9e4d-f885d363cce2")]
+[assembly: Guid("263a73c7-bdf9-49fd-81ed-bd112136f156")]
 
 // Version information for an assembly consists of the following four values:
 //
