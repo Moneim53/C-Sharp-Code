24b043df638d3d03807dded7851b25e7cd480057
Moneim53 <https://github.com/Moneim53>
2018-11-26 13:40:14 +0200

    https://github.com/Moneim53/C-Sharp-Code/tree/Elephant_Swap

--- ElephantSwap/Program.cs
+++ ElephantSwap/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Flashy_
+namespace ElephantSwap
 {
     static class Program
     {
--- ElephantSwap/Properties/Settings.Designer.cs
+++ ElephantSwap/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Flashy_.Properties
+namespace ElephantSwap.Properties
 {
 
 
